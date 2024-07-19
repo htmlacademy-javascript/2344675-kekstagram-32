1d0703e490179257d74d9306207cd7a768876b6a
Dar-Suvorov <suvorov.2016@yandex.ru>
2024-07-19 09:14:31 +0300

    (fix) Прописывание alt

--- js/draw-thumbs.js
+++ js/draw-thumbs.js
@@ -7,7 +7,7 @@ const drawThumbs = (source) => {
   for (let i = 0; i < source.length; i++) {
     const filledPost = postTemplate.cloneNode(true); // шаблон фото для наполнения, текущего - для заполнения
     filledPost.querySelector('.picture__img').src = source[i].url;
-    filledPost.querySelector('.picture__info').alt = source[i].description;
+    filledPost.querySelector('.picture__img').alt = source[i].description;
     filledPost.querySelector('.picture__likes').textContent = source[i].likes;
     filledPost.querySelector('.picture__comments').textContent = source[i].comments.length;
     posts.appendChild(filledPost);
