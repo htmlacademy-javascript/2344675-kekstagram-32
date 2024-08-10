d8e9c77b315505006788cef9362c9e50510a284f
Dar-Suvorov <suvorov.2016@yandex.ru>
2024-08-10 16:51:28 +0300

    11.14 поиск бага

--- js/upload-form-sending.js
+++ js/upload-form-sending.js
@@ -2,18 +2,6 @@ const SERVER_URL_UPLOAD = 'https://32.javascript.htmlacademy.pro/kekstagram';
 const submitBtn = document.querySelector('.img-upload__submit');
 const submitBtnDefaultText = submitBtn.textContent;
 
-const submitBtnBlock = () => {
-  submitBtn.disabled = 'true';
-  // console.log('Кнопка заблокирована');
-  submitBtn.textContent = 'Отправляется...';
-};
-
-const submitBtnUnblock = () => {
-  submitBtn.disabled = 'false';
-  // console.log('Кнопка разблокирована');
-  submitBtn.textContent = submitBtnDefaultText;
-};
-
 const sendingMsgSuccess = document.querySelector('#success').content.cloneNode(true);
 const sendingMsgSuccessBtn = sendingMsgSuccess.querySelector('.success__button');
 
@@ -21,10 +9,39 @@ const sendingMsgError = document.querySelector('#error').content.cloneNode(true)
 const sendingMsgErrorBtn = sendingMsgError.querySelector('.error__button');
 
 
+console.log('Изначальное состояние:');
+console.log(submitBtn);
+
+const submitBtnBlock = () => {
+  console.log('============================');
+  console.log('submitBtnBlock');
+  console.log(submitBtn);
+  submitBtn.disabled = 'true';
+  console.log('Кнопка заблокирована');
+  submitBtn.textContent = 'Отправляется...';
+  console.log(submitBtn);
+};
+
+const submitBtnUnblock = () => {
+  console.log('============================');
+  console.log('submitBtnUnblock');
+  submitBtn.disabled = 'false';
+  console.log(submitBtn);
+  console.log('Кнопка разблокирована');
+  submitBtn.textContent = submitBtnDefaultText;
+  console.log(submitBtn);
+};
+
 export const proceedUpload = (evt) => {
   const formData = new FormData(evt.target);
 
+  console.log('Начали proceedUpload');
+  console.log(submitBtn);
+  console.log('блокируем кнопку');
   submitBtnBlock();
+  console.log(submitBtn);
+
+  console.log('Выполняем fetch...');
   fetch(SERVER_URL_UPLOAD,
     {
       method: 'POST',
@@ -38,8 +55,8 @@ export const proceedUpload = (evt) => {
         sendingMsgSuccessBtn.addEventListener('click', () => {
           // console.log('разблокируем кнопку');
           // console.log(submitBtn);
-          submitBtnUnblock();
           sendingMsgSuccessPlate.remove();
+          submitBtnUnblock();
         });
       }
     })
@@ -47,11 +64,11 @@ export const proceedUpload = (evt) => {
       document.body.appendChild(sendingMsgError);
       const sendingMsgErrorPlate = document.querySelector('.error');
       sendingMsgErrorBtn.addEventListener('click', () => {
+        submitBtnUnblock();
         sendingMsgErrorPlate.remove();
       });
       // console.log('разблокируем кнопку');
       // console.log(submitBtn);
-      submitBtnUnblock();
     })
     // .finally(() => {
     // submitBtnUnblock();
