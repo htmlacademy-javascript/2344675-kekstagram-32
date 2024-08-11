ddf4c929d9ac405957d26a090fb4fcec1e9a1ef7
Dar-Suvorov <suvorov.2016@yandex.ru>
2024-08-11 14:27:01 +0300

    11.14

--- js/upload-form-sending.js
+++ js/upload-form-sending.js
@@ -1,21 +1,14 @@
 const SERVER_URL_UPLOAD = 'https://32.javascript.htmlacademy.pro/kekstagram';
 const submitBtn = document.querySelector('.img-upload__submit');
 const submitBtnDefaultText = submitBtn.textContent;
+const submitBtnProcessingtText = 'Отправляется...';
 
-const sendingMsgSuccessTemplate
- = document.querySelector('#success').content;
-const sendingMsgSuccess
- = sendingMsgSuccessTemplate.cloneNode(true);
-const sendingMsgSuccessBtn
- = sendingMsgSuccess.querySelector('.success__button');
 
-const sendingMsgError = document.querySelector('#error').content.cloneNode(true);
-const sendingMsgErrorBtn = sendingMsgError.querySelector('.error__button');
 
 
 const blockSubmitBtn = () => {
   submitBtn.disabled = true;
-  submitBtn.textContent = 'Отправляется...';
+  submitBtn.textContent = submitBtnProcessingtText;
 };
 
 const unblockSubmitBtn = () => {
@@ -24,37 +17,6 @@ const unblockSubmitBtn = () => {
 };
 
 export const proceedUpload = (evt) => {
-  console.log('Запущен proceedUpload');
-  const formData = new FormData(evt.target);
-
-  blockSubmitBtn();
-
-  fetch(SERVER_URL_UPLOAD,
-    {
-      method: 'POST',
-      body: formData
-    }
-  )
-    .then((response) => {
-      if (response.ok) {
-        document.body.appendChild(sendingMsgSuccess);
-        const sendingMsgSuccessPlate = document.querySelector('.success');
-        // console.log(sendingMsgSuccessPlate); // ??? На второй итерации возвращает null
-        sendingMsgSuccessBtn.addEventListener('click', () => {
-          sendingMsgSuccessPlate.remove();
-        });
-        unblockSubmitBtn();
-      }
-    })
-    .catch(() => {
-      document.body.appendChild(sendingMsgError);
-
-      const sendingMsgErrorPlate = document.querySelector('.error');
-      sendingMsgErrorBtn.addEventListener('click', () => {
-        sendingMsgErrorPlate.remove();
-      });
-      unblockSubmitBtn();
-    });
-
+  console.log('proceedUpload:');
 };
 
